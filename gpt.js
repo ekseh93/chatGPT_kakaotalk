7b6fec4997a1b56581c04fb73c902ef845015648
ekseh93 <60168922+ekseh93@users.noreply.github.com>
2023-05-06 01:25:13 +0900

    Update gpt.js

--- gpt.js
+++ gpt.js
@@ -20,7 +20,7 @@ function getResponse(msg) {
   "presence_penalty": 2.0};   // 값이 높을수록 단어를 반복하지 않음
   try {
     let response = org.jsoup.Jsoup.connect("https://api.openai.com/v1/chat/completions").header("Content-Type", "application/json").header("Authorization", "Bearer " + key).requestBody(JSON.stringify(data)).ignoreContentType(true).ignoreHttpErrors(true).timeout(1000000).post();
-    result1 = JSON.parse(response.text());      // timeout(1000000) : 과부하가 일어나지않게 1000초 지연 설정
+    result1 = JSON.parse(response.text());      // timeout(1000000) :  1000초 지연 설정
     result = result1.choices[0].message.content;
   }  catch (e) {
   result = e;
